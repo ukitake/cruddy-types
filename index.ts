b8e9797b10ce5f306b7c171edd929cff0c541f96
Sam Mason <sammason3388@gmail.com>
2024-05-06 14:40:12 -0500

    updating supported sql alchemy ops (#14)
    Co-authored-by: Sam Mason <sam.mason@blackcape.io>

--- index.ts
+++ index.ts
@@ -1,4 +1,28 @@
-const SQL_OPS = ['*in_', '*eq', '*neq', '*contains', '*icontains'] as const;
+const SQL_OPS = [
+  '*in_',
+  '*eq',
+  '*neq',
+  '*contains',
+  '*icontains',
+  '*websearch_to_tsquery',
+  '*gt',
+  '*gte',
+  '*lt',
+  '*lte',
+  '*like',
+  '*contained_by',
+  '*has_key',
+  '*has_all',
+  '*intersects',
+  '*intersects_nd',
+  '*same',
+  '*above',
+  '*below',
+  '*datetime',
+  '*datetime_naive',
+  '*startswith',
+  '*endswith'
+] as const;
 
 // request params
 export type BaseAuthRequestParams = {
@@ -39,7 +63,7 @@ type NotGrouping<TModel> = {
 
 export type LogicalGrouping<TModel> = OneOf<[AndGrouping<TModel>, OrGrouping<TModel>, NotGrouping<TModel>]>;
 
-type Value = string | number | null;
+type Value = string | number | boolean | null;
 
 type SqlOps = (typeof SQL_OPS)[number];
 
@@ -47,15 +71,42 @@ type SqlOp<S extends SqlOps> = {
   [key in Exclude<SqlOps, Exclude<SqlOps, S>>]: Value | Value[];
 };
 
-export type Logic = OneOf<[SqlOp<'*in_'>, SqlOp<'*eq'>, SqlOp<'*neq'>, SqlOp<'*contains'>, SqlOp<'*icontains'>]>;
+export type Logic = OneOf<
+  [
+    SqlOp<'*in_'>,
+    SqlOp<'*eq'>,
+    SqlOp<'*neq'>,
+    SqlOp<'*contains'>,
+    SqlOp<'*icontains'>,
+    SqlOp<'*websearch_to_tsquery'>,
+    SqlOp<'*gt'>,
+    SqlOp<'*gte'>,
+    SqlOp<'*lt'>,
+    SqlOp<'*lte'>,
+    SqlOp<'*like'>,
+    SqlOp<'*contained_by'>,
+    SqlOp<'*has_key'>,
+    SqlOp<'*has_all'>,
+    SqlOp<'*intersects'>,
+    SqlOp<'*intersects_nd'>,
+    SqlOp<'*same'>,
+    SqlOp<'*above'>,
+    SqlOp<'*below'>,
+    SqlOp<'*datetime'>,
+    SqlOp<'*datetime_naive'>,
+    SqlOp<'*startswith'>,
+    SqlOp<'*endswith'>
+  ]
+>;
 
 // (SM) make a type where all keys of T with '.${any string}'following them are valid keys
 type Dots<T> = {
-  [K in keyof T & string as `${K}.${string}`]: T[K]
+  [K in keyof T & string as `${K}.${string}`]: T[K];
 };
 
 export type Clause<TModel> = {
-  [key in keyof (TModel & Dots<TModel>)]: Record<key, Logic> & Partial<Record<Exclude<keyof TModel, key>, never>> extends infer O
+  [key in keyof (TModel & Dots<TModel>)]: Record<key, Logic> &
+    Partial<Record<Exclude<keyof TModel, key>, never>> extends infer O
     ? Expand<O>
     : never;
 }[keyof (TModel & Dots<TModel>)];
